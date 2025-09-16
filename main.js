b9d685d01e5705231a7dbb2b8ef13fb2cb641a4c
Santiago Sica <santisica29@gmail.com>
2025-09-15 21:26:53 -0300

    Make operate fn

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+let num1 = 0;
+let num2 = 0;
+let op = "";
+
 function add(n1, n2) {
   return n1 + n2;
 }
 
-function substract(n1, n2) {
+function subtract(n1, n2) {
   return n1 - n2;
 }
 
@@ -12,4 +16,23 @@ function multiply(n1, n2) {
 
 function divide(n1, n2) {
     return n1 / n2;
+}
+
+function operate(operator, n1, n2) {
+    switch (operator){
+        case '+':
+            add(n1, n2);
+            break;
+        case '-':
+            subtract(n1, n2);
+            break;
+        case '*':
+            multiply(n1, n2);
+            break;
+        case '/':
+            divide(n1, n2);
+            break;
+        default:
+            break;
+    }
 }
