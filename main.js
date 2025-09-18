6e49c24c920f50209cbe36140b3db9953c597c0f
Santiago Sica <santisica29@gmail.com>
2025-09-18 12:01:01 -0300

    separate buttons of nums from oeprators to figure out the logic of the app

--- main.js
+++ main.js
@@ -1,24 +1,56 @@
-let num1 = 0;
-let num2 = 0;
-let op = "";
+let num1 = '';
+let num2 = '';
+let op = '';
+let result = '';
+let operators = ["+", "-", "*", "/"];
 
-let displayScreen = document.querySelector('.display p');
-let buttons = document.querySelectorAll('.btn');
+let displayScreen = document.querySelector(".display p");
+let buttons = document.querySelectorAll(".btn");
+let numsBtn = document.querySelectorAll(".btn-num");
+let opBtn = document.querySelectorAll(".btn-op");
 
-buttons.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        let value = e.currentTarget.textContent;
-
-        populateDisplay(e);
-        num1 = value.textContent;
-    })
-    
-})
-
-function populateDisplay(e){
+numsBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
     let value = e.currentTarget.textContent;
-
     displayScreen.textContent += value;
+
+    if (num1 === '' || (num1 != '' && op === '')) {
+      num1 += value;
+    } else {
+      num2 += value;
+    }
+
+    console.log(num1);
+    console.log(op);
+    console.log(num2);
+
+  });
+});
+
+opBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (num1 != '' && num2 != ''){
+        result = operate(op, num1, num2);
+
+        displayScreen.textContent = result;
+    }
+
+    let value = e.currentTarget.textContent;
+    displayScreen.textContent += ' ' + value + ' ';
+
+    op = value;
+
+    console.log(num1);
+    console.log(op);
+    console.log(num2);
+    console.log(result)
+  });
+});
+
+function populateDisplay(e) {
+  let value = e.currentTarget.textContent;
+
+  displayScreen.textContent += value;
 }
 
 function add(n1, n2) {
@@ -34,24 +66,24 @@ function multiply(n1, n2) {
 }
 
 function divide(n1, n2) {
-    return n1 / n2;
+  return n1 / n2;
 }
 
 function operate(operator, n1, n2) {
-    switch (operator){
-        case '+':
-            add(n1, n2);
-            break;
-        case '-':
-            subtract(n1, n2);
-            break;
-        case '*':
-            multiply(n1, n2);
-            break;
-        case '/':
-            divide(n1, n2);
-            break;
-        default:
-            break;
-    }
+    let result = '';
+  switch (operator) {
+    case "+":
+      result = add(n1, n2);
+      break;
+    case "-":
+      result = subtract(n1, n2);
+      break;
+    case "*":
+      result = multiply(n1, n2);
+      break;
+    case "/":
+      result = divide(n1, n2);
+      break;
+  }
+  return result;
 }
