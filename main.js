ce2c709371c9bbd46d6abe544421559733a142bc
Santiago Sica <santisica29@gmail.com>
2025-09-20 23:07:51 -0300

    made fn for = btn

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
 // TODO:
 // ver una forma segura de pasar de string a number
-// evitar que se ingresen dos puntos seguidos
 // usar data-value para los valores
 // AL TERMINAR Logica
 // mejorar el layout y hacerlo todo negro con grises y celestes
 // ver manera de reusar algunas funciones
 
-let num1 = '';
-let num2 = '';
-let op = '';
-let result = '';
+let num1 = "";
+let num2 = "";
+let op = "";
+let result = "";
 
 let displayScreen = document.querySelector(".display p");
 let numsBtn = document.querySelectorAll(".btn-num");
 let opBtn = document.querySelectorAll(".btn-op");
-let eraseBtn = document.querySelector('.btn-c');
-let pointBtn = document.querySelector('.btn-point');
+let eraseBtn = document.querySelector(".btn-c");
+let pointBtn = document.querySelector(".btn-point");
+let equalBtn = document.querySelector(".btn-equal");
 
-eraseBtn.addEventListener('click', erase);
-pointBtn.addEventListener('click', managePoint)
+eraseBtn.addEventListener("click", erase);
+pointBtn.addEventListener("click", managePoint);
+equalBtn.addEventListener("click", completeOperation);
 
 numsBtn.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     let value = e.currentTarget.textContent;
     displayScreen.textContent += value;
 
-    if (num1 === '' || (num1 != '' && op === '')) {
+    if (num1 === "" || (num1 != "" && op === "")) {
       num1 += value;
     } else {
       num2 += value;
@@ -34,57 +35,74 @@ numsBtn.forEach((btn) => {
     console.log(num1);
     console.log(op);
     console.log(num2);
-
   });
 });
 
 opBtn.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    if (num1 != '' && num2 != ''){
-        num1 = Number(num1);
-        num2 = Number(num2);
-        result = operate(op, num1, num2);
+    if (num1 != "" && num2 != "") {
+      num1 = Number(num1);
+      num2 = Number(num2);
+      result = operate(op, num1, num2);
 
-        num1 = result;
-        num2 = '';
+      num1 = result;
+      num2 = "";
 
-        displayScreen.textContent = result;
+      displayScreen.textContent = result;
     }
 
     let value = e.currentTarget.textContent;
-    displayScreen.textContent += ' ' + value + ' ';
+    displayScreen.textContent += " " + value + " ";
 
     op = value;
 
     console.log(num1);
     console.log(op);
     console.log(num2);
-    console.log(result)
+    console.log(result);
   });
 });
 
-function managePoint(){
-  if ((num1.toString().split('').includes('.') && num1 !== result) || num2.split('').includes('.')){
-    console.log(num1 === result);
-    alert('invalid option');
+function completeOperation() {
+  if (num2 === "") {
     return;
   }
 
-  if (op === ''){
-    num1 += '.'
-  } else {
-    num2 += '.'
+  num1 = Number(num1);
+  num2 = Number(num2);
+  result = operate(op, num1, num2);
+
+  num1 = result;
+  num2 = "";
+
+  displayScreen.textContent = result;
+}
+
+function managePoint() {
+  if (
+    (num1.toString().split("").includes(".") && num1 !== result) ||
+    num2.split("").includes(".")
+  ) {
+    console.log(num1 === result);
+    alert("invalid option");
+    return;
   }
 
-  displayScreen.textContent += '.';
+  if (op === "") {
+    num1 += ".";
+  } else {
+    num2 += ".";
+  }
+
+  displayScreen.textContent += ".";
 }
 
 function erase() {
-  num1 = '';
-  num2 = '';
-  op = '';
-  result = '';
-  displayScreen.textContent = '';
+  num1 = "";
+  num2 = "";
+  op = "";
+  result = "";
+  displayScreen.textContent = "";
 }
 function populateDisplay(e) {
   let value = e.currentTarget.textContent;
@@ -109,7 +127,7 @@ function divide(n1, n2) {
 }
 
 function operate(operator, n1, n2) {
-    let result = '';
+  let result = "";
   switch (operator) {
     case "+":
       result = add(n1, n2);
